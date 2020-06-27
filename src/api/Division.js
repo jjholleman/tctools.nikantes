f73ed15fce287c6f6e4710685a7400b4e58eb7bf
Jordi Holleman <jordiholleman@gmail.com>
2020-06-27 21:15:43 +0200

    [divisions] added average age limits info and a getter for the info

--- src/api/Division.js
+++ src/api/Division.js
@@ -1,43 +1,72 @@
+/* eslint-disable no-console */
 export default {
     divisions: [
         {
             name: "Senioren",
+            avg_age: {
+                min: 16,
+                max: 9999,
+            },
             max_age: 9999,
             min_age: 19,
             indexId: 7,
         },
         {
             name: "A",
+            avg_age: {
+                min: 15.1,
+                max: 18.0,
+            },
             max_age: 19,
             min_age: 16,
             indexId: 6,
         },
         {
             name: "B",
+            avg_age: {
+                min: 13.1,
+                max: 15.0,
+            },
             max_age: 16,
             min_age: 14,
             indexId: 5,
         },
         {
             name: "C",
+            avg_age: {
+                min: 11.1,
+                max: 13.0,
+            },
             max_age: 14,
             min_age: 12,
             indexId: 4,
         },
         {
             name: "D",
+            avg_age: {
+                min: 9.1,
+                max: 11.0,
+            },
             max_age: 12,
             min_age: 10,
             indexId: 3,
         },
         {
             name: "E",
+            avg_age: {
+                min: 7.1,
+                max: 9.0,
+            },
             max_age: 10,
             min_age: 8,
             indexId: 2,
         },
         {
             name: "F",
+            avg_age: {
+                min: 5.1,
+                max: 7.0,
+            },
             max_age: 8,
             min_age: 0,
             indexId: 1,
@@ -46,8 +75,15 @@ export default {
     getAllDivisions() {
         return this.divisions;
     },
+    findDivisionByLetter(division, identifier) {
+        return division.name === identifier
+    },
     getDivisionMaxAge(divisionLetter) {
-        let division = this.divisions.find(division => {return division.name === divisionLetter});
+        let division = this.divisions.find(division => this.findDivisionByLetter(division, divisionLetter));
         return division.max_age;
+    },
+    getDivisionAgeLimit(divisionLetter) {
+        let division = this.divisions.find(division => this.findDivisionByLetter(division, divisionLetter));
+        return division.avg_age;
     }
 }
